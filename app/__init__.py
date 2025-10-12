df103c4568c2fa0a358ec1c9128a533d57e33c96
gilianfavour <gilianfavour06gmail.com>
2025-10-12 08:04:09 +0300

    controllers

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask_cors import CORS
 from app.extensions import db,migrate
+from flask_jwt_extended import JWTManager
 
 from app.models.userModel import User
 from app.models.auditModel import AuditLog
@@ -10,6 +12,12 @@ from app.models.transactionModel import Transaction
 from app.models.savingModel import Saving
 
 from app.controllers.AuthController import auth_bp
+from app.controllers.saccoController import sacco_bp
+from app.controllers.DashBoardController import dashboard_bp
+from app.controllers.SavingController import saving_bp
+from app.controllers.UserContoller import user_bp
+from app.controllers.TransactionController import transaction_bp
+from app.controllers.NotificationController import notification_bp
 
 
 #application factory function
@@ -17,13 +25,23 @@ def create_app():
     
     #app instance
     app = Flask(__name__)
+    CORS(app, origins="http://localhost:3000")
     app.config.from_object('config.Config')
     
     db.init_app(app)
     migrate.init_app(app,db)
 
+    # JWT setup
+    jwt = JWTManager(app)
+
 
     app.register_blueprint(auth_bp, url_prefix='/api/auth')
+    app.register_blueprint(sacco_bp, url_prefix='/api/saccos')
+    app.register_blueprint(dashboard_bp, url_prefix='/api/dashboard')
+    app.register_blueprint(saving_bp, url_prefix='/api/savings')
+    app.register_blueprint(user_bp, url_prefix='/api/users')
+    app.register_blueprint(transaction_bp, url_prefix='/api/transactions')
+    app.register_blueprint(notification_bp, url_prefix='/api/notifications')
 
     @app.route("/")
     def home():
--- config.py
+++ config.py
@@ -4,3 +4,9 @@ class Config:
      SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/saccoconnect_db'
      JWT_SECRET_KEY = "SACCOCOnnect" 
      JWT_EXPIRATION_DELTA = timedelta(minutes=10)
+     
+     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # JWT settings
+     JWT_SECRET_KEY = 'your-jwt-secret-key'  # 🔑 must be set
+     JWT_TOKEN_LOCATION = ['headers']        # where to look for the token
